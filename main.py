f8712503d3264b91504325f730519c45ef38bceb
Terrell Wade <terrywade634@gmail.com>
2023-05-19 09:49:20 -0700

    Destinations are set for Day Trips

---
+++ main.py
@@ -0,0 +1,10 @@
+import random
+
+
+destinations = ["New York", "Los Angeles", "Miami", "Tampa Bay", "Las Vegas"]
+user_destination = random.choice(destinations) 
+#These are lists that should be localized to the destinations on destinations lists
+#restaurants = [] 
+#transportation_types = []
+#entertainment_items = []
+# I need a function that will take a destination and give me possible restaurants and entertainment.
