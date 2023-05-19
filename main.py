fa3a81815d8d7770a6f616303148b7a4317c974c
TerryW624 <terrywade634@gmail.com>
2023-05-19 13:51:51 -0700

    Separated function into two for user end reroll

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ import random
 
 
 destinations = ["New York", "Los Angeles", "Miami", "Tampa Bay", "Las Vegas"]
-user_destination = random.choice(destinations)
 #list directory for entertainment and food by city
-new_york_restaurants = ["Keens Steakhou se", "Don Peppe", "Balthazar", "Katz's Deli", "Bemelmans Bar at the Carlyle"]
+new_york_restaurants = ["Keens Steakhouse", "Don Peppe", "Balthazar", "Katz's Deli", "Bemelmans Bar at the Carlyle"]
 new_york_entertainments = ["Visit the Statue of Liberty", "Catch a Show On Broadway", "SUMMIT One Vanderbilt Experience", "Manhattan Island Helicopter Tour", "1-Hour Cruise Around Statuteof Liberty & Ellis Island"]
 los_angeles_restaurants = ["Philipe the Original", "Musso & Frank Grill", "Pacific Dining Car", "Tam O'Shanter", "El Paseo Inn"]
 los_angeles_entertainments = ["Universal Studios Hollywood", "Disney Land", "Knott's Berry Farm", "Aquarium of the Pacific", "The Official Hollywood Sign Walking Tour"]
@@ -14,27 +13,39 @@ tampa_bay_restaurants = ["Bern's Steak House", "Bob Heilman's Beachcomber", "Caf
 tampa_bay_entertainments = ["Tiki Boat", "Guided Tampa Tour", "Dolphin Sightseeing Cruise from Tampa", "Tiki Boat Tour", "Tampa Histry Cruise"]
 las_vegas_restaurants = ["Eiffel Tower Restaurant", "Sterling Brunch at Bally's", "Guy Savoy", "Mizumi", "Joë Robuchon"]
 las_vegas_entertainments = ["Cirque Du Soleil", "David Copperfield at the MGM Grand Hotel", "Big Bus Las Vegas Open Top Night Tour", "Shark Reef", "The High Roller"]
-# sets the variables user_restaurant and user_entertainement based on a destination
-def get_restaurant_and_entertainment_by_city(destination):
-    #placeholder variables
+user_destination = random.choice(destinations)
+print(f"Your destination will be {user_destination}")
+# sets the variables user_restaurant based on a destination
+def get_restaurant_by_city(destination):
+    #placeholder variable
     user_restaurant = ""
-    user_entertainment = ""
     if destination == "New York":
         user_restaurant = random.choice(new_york_restaurants)
-        user_entertainment = random.choice(new_york_entertainments)
     elif destination == "Los Angeles":
         user_restaurant = random.choice(los_angeles_restaurants)
-        user_entertainment = random.choice(los_angeles_entertainments)
     elif destination == "Miami":
         user_restaurant = random.choice(miami_restaurants)
-        user_entertainment = random.choice(miami_entertainments)
     elif destination == "Tampa Bay":
         user_restaurant = random.choice(tampa_bay_restaurants)
-        user_entertainment = random.choice(tampa_bay_entertainments)
     elif destination == "Las Vegas":
         user_restaurant = random.choice(las_vegas_restaurants)
-        user_entertainment = random.choice(las_vegas_entertainments)
+    print(f"Your restaurant is {user_restaurant}")  
+def get_entertainment_by_city(destination):
+    #placeholder variable
+    user_entertainment = ""
+    if destination == "New York":
+        user_entertainment = random.choice(new_york_entertainments)
+    elif destination == "Los Angeles":
+        user_entertainment = random.choice(los_angeles_entertainments)
+    elif destination == "Miami":
+        user_entertainment = random.choice(miami_entertainments)
+    elif destination == "Tampa Bay":
+        user_entertainment = random.choice(tampa_bay_entertainments)
+    elif destination == "Las Vegas":
+        user_entertainment = random.choice(las_vegas_entertainments) 
+    print(f"You should have fun at {user_entertainment}")
 transportations = ["Plane", "Train", "Car", "Bus"]
 user_transportation = random.choice(transportations)
-get_restaurant_and_entertainment_by_city(user_destination)
-
+print(f"You should arrive and depart by {user_transportation}")
+get_restaurant_by_city(user_destination)
+get_entertainment_by_city(user_destination)
